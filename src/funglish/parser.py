27eff161d5625394eea539af35f05b9b5b4bac5f
Sam Naylor <sam.naylor@tharsus.co.uk>
2020-11-06 13:36:11 +0000

    grammar snippets inside functions in parser

--- src/funglish/parser.py
+++ src/funglish/parser.py
@@ -5,29 +5,44 @@ class Parser:
 		self.tree = []
 
 	def start(self):
+		' start : toplevel* '
 		while self.tokens:
-			top = self.tokens[0]
-
-			if top.type == 'IDENTIFIER':
-				self.function()
-			else:
-				self.tls()
-
+			self.toplevel()
+		
 		return self.tree
 
+
+	def toplevel(self):
+		'''
+		toplevel : function
+				 | tls
+		'''
+		top = self.tokens[0]
+
+		if top.type == 'IDENTIFIER':
+			self.tree.append(self.function())
+		else:
+			self.tree.append(self.tls())
+
 	def function(self):
+		' function : IDENTIFIER take_statement '
 		name = self.tokens.pop(0).value
 		self.tree.append(["FUNCTION", name, self.take_statement()])
 
 	def tls(self):
+		' tls : statement '
+		# TLS simply means top level statement
+		# It's needed for the code-generator
 		self.tree.append(["TLS", self.statement()])
 
 	def take_statement(self):
+		' take_statement : "take" statement '
 		assert self.tokens[0].type == "TAKE", f"Expected TAKE, got {self.tokens[0]}"
 		self.tokens.pop(0)
 		return self.statement()
 
 	def statement(self):
+		' statement : statement_part [(("," statement_part)*)] "." '
 		stmt = [self.statement_part()]
 
 		while self.tokens[0].type != "EOS":
@@ -42,6 +57,11 @@ class Parser:
 		return stmt
 
 	def statement_part(self):
+		'''
+		statement_part : maths
+					   | show
+					   | expression	
+		'''
 		if self.tokens[0].type == "SHOW":
 			self.tokens.pop(0)
 			return ["SHOW"]
@@ -51,6 +71,12 @@ class Parser:
 			return self.expression()
 
 	def maths(self):
+		'''
+		maths : "add" expression
+			  | "subtract" expression
+			  | "multiply" "by" expression
+			  | "divide" "by" expression
+		'''
 		op = self.tokens.pop(0)
 		if op.type in ("MULTIPLY", "DIVIDE"):
 			assert self.tokens[0].type == "BY", f"Expected BY, got {self.tokens[0]}"
@@ -59,6 +85,12 @@ class Parser:
 		return [op.type, self.expression()]
 
 	def expression(self):
+		'''
+		expression : NUMBER
+				   | IDENTIFIER
+				   | "run" expression
+				   | "load" expression
+		'''
 		if self.tokens[0].type == "NUMBER":
 			return ["NUMBER", self.tokens.pop(0).value]
 		elif self.tokens[0].type == "IDENTIFIER":
